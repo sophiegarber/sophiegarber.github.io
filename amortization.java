0dcd253a8ca256f0aa3b366a80dcaff7e5d0a7d1
sophiegarber <94139607+sophiegarber@users.noreply.github.com>
2023-10-25 08:15:23 -0400

    Create amortization.java

---
+++ amortization.java
@@ -0,0 +1,12 @@
+import java.util.*
+import java.io.*
+  
+//main body of print statements
+public static void main(String[] args) {
+
+//ask user for input
+  System.out.println("Enter your total loan amount: ");
+  int loan = in.nextLin();
+  System.out.println("loan is: " + loan);
+  
+}
